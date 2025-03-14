15eb05331c564e9ed6c39421e0222f5080c42420
AngeloMakory <angelomakory@gmail.com>
2025-03-14 12:45:20 +0300

    fix:homepage mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,21 @@ import Header from "./_components/header";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-4 pb-16 gap-8 sm:p-8 md:p-16 lg:p-20 md:gap-16 font-[family-name:var(--font-geist-sans)]">
       
       <Header />
       
       <main className="flex flex-col gap-8 row-start-2 items-center text-center">
         <div>
-          <h1 className="text-6xl font-bold animate-fade-in font-[family-name:var(--font-geist-mono)] bg-gradient-to-r from-green-600 via-yellow-500 to-blue-500 text-transparent bg-clip-text">
+          <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-6xl font-bold animate-fade-in font-[family-name:var(--font-geist-mono)] bg-gradient-to-r from-green-600 via-yellow-500 to-blue-500 text-transparent bg-clip-text">
             Hello, I am Angelo Makory
           </h1>
-          <br></br>
-          <br></br>
-          <p className="text-2xl font-sm animate-fade-in font-[family-name:var(--font-geist-mono)] bg-gradient-to-r from-green-600 via-yellow-500 to-blue-500 text-transparent bg-clip-text">
-            I am a very passionate Full Stack developer and data enthusiast fueled by the commitment to provide the best professional services in my qualifications.
-          </p>
+          <div className='my-3 sm:my-4 md:my-6'>
+            <p className="text-md sm:text-lg md:text-xl lg:text-2xl font-sm animate-fade-in font-[family-name:var(--font-geist-mono)] bg-gradient-to-r from-green-600 via-yellow-500 to-blue-500 text-transparent bg-clip-text">
+              I am a very passionate Full Stack developer and data enthusiast fueled by the commitment to provide the best professional services in my qualifications.
+            </p>
+          </div>
+          
         </div>
         
         {/* <Image
